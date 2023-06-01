78075b82cea85adec71b759ed188933ddae35339
Alexander Drakon <drakon.a.v@gmail.com>
2023-06-02 00:32:02 +0300

    fix: замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,28 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.form');
+let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('#name');
 let jobInput = formElement.querySelector('#job');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
+    nameInput.defaultValue = profileName.textContent;
+    jobInput.defaultValue = profileJob.textContent;
 }
   
 function handleFormSubmit (evt) {
     evt.preventDefault();
     let name = nameInput.value;
     let job = jobInput.value;
-    let name_heading = document.querySelector('.profile__name');
-    let job_heading = document.querySelector('.profile__job');
-    name_heading.textContent = name;
-    job_heading.textContent = job;
+    profileName.textContent = name;
+    profileJob.textContent = job;
     togglePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
 editButton.addEventListener('click', togglePopup);
 closeButton.addEventListener('click', togglePopup);
+formElement.addEventListener('submit', handleFormSubmit);
