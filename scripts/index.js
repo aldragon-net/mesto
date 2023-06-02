ea8413c19028a505d4f5417c080397bd606446e3
Alexander Drakon <drakon.a.v@gmail.com>
2023-06-02 16:48:27 +0300

    fix: замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('#name');
-let jobInput = formElement.querySelector('#job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('#input-name');
+let jobInput = formElement.querySelector('#input-job');
 
 function togglePopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popup.classList.toggle('popup_opened');
-    nameInput.defaultValue = profileName.textContent;
-    jobInput.defaultValue = profileJob.textContent;
 }
-  
+
 function handleFormSubmit (evt) {
     evt.preventDefault();
     let name = nameInput.value;
