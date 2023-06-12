4e33fee986a1ea8c12297139f5cb614d0a7dc22b
Alexander Drakon <drakon.a.v@gmail.com>
2023-06-12 23:31:55 +0300

    feat: working alpha

--- scripts/index.js
+++ scripts/index.js
@@ -37,9 +37,11 @@ function addCard (card) {
     const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
     placeElement.querySelector('.place__name').textContent = card.name;
     placeElement.querySelector('.place__image').src = card.link;
+    placeElement.querySelector('.place__image').alt = `фотография места «${card.name}»`;
+    placeElement.querySelector('.place__image').addEventListener('click', openImage);
     placeElement.querySelector('.place__like-icon').addEventListener('click', toggleLike);
     placeElement.querySelector('.place__delete-icon').addEventListener('click', deleteCard);
-    places.append(placeElement); 
+    places.prepend(placeElement); 
 }
 
 function addInitialCards (initialCards) {
@@ -51,31 +53,77 @@ const placeTemplate = document.querySelector('#place').content;
 const places = document.querySelector('.places');
 addInitialCards(initialCards);
 
-let editButton = document.querySelector('.profile__edit-button');
+let editProfileButton = document.querySelector('.profile__edit-button');
+let addPlaceButton = document.querySelector('.profile__add-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('#input-name');
-let jobInput = formElement.querySelector('#input-job');
+let popupImage = document.querySelector('.popup__image');
+let popupImageCaption = document.querySelector('.popup__image-caption');
 
-function togglePopup() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-    popup.classList.toggle('popup_opened');
+let profilePopup = document.querySelector('#profile-popup');
+let placePopup = document.querySelector('#place-popup');
+let picturePopup = document.querySelector('#picture-popup');
+let closeButtons = document.querySelectorAll('.popup__close');
+let profileForm = document.querySelector('#profile-form');
+let profileNameInput = profileForm.querySelector('#input-profile-name');
+let profileJobInput = profileForm.querySelector('#input-profile-job');
+
+let placeForm = document.querySelector('#place-form');
+let placeNameInput = placeForm.querySelector('#input-place-name');
+let placePhotoInput = placeForm.querySelector('#input-place-photo');
+
+
+function togglePopup(popup) {
+  popup.classList.toggle('popup_opened');
 }
 
-function handleFormSubmit (evt) {
+function openProfileForm() {
+    profileNameInput.value = profileName.textContent;
+    profileJobInput.value = profileJob.textContent;
+    togglePopup(profilePopup);
+}
+
+function openPlaceForm() {
+  placeNameInput.value = '';
+  placePhotoInput.value = '';
+  togglePopup(placePopup);
+}
+
+function openImage(evt) {
+  image = evt.target;
+  caption = image.parentElement.querySelector('.place__info .place__name').textContent;
+  popupImage.src = image.src;
+  popupImageCaption.textContent = caption;
+  togglePopup(picturePopup)
+}
+
+function closeForm(evt) {
+  popup = evt.target.closest('.popup');
+  togglePopup(popup);
+}
+
+function handleProfileFormSubmit (evt) {
     evt.preventDefault();
-    let name = nameInput.value;
-    let job = jobInput.value;
+    let name = profileNameInput.value;
+    let job = profileJobInput.value;
     profileName.textContent = name;
     profileJob.textContent = job;
-    togglePopup();
+    togglePopup(profilePopup);
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+function handlePlaceFormSubmit (evt) {
+  evt.preventDefault();
+  let newCard = {};
+  newCard.name = placeNameInput.value;
+  newCard.link = placePhotoInput.value;
+  addCard(newCard);
+  togglePopup(placePopup);
+}
+
+editProfileButton.addEventListener('click', openProfileForm);
+addPlaceButton.addEventListener('click', openPlaceForm);
+
+closeButtons.forEach(elem => elem.addEventListener('click', closeForm));
+profileForm.addEventListener('submit', handleProfileFormSubmit);
+placeForm.addEventListener('submit', handlePlaceFormSubmit);
